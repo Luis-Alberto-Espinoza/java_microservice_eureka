ba993ffd4030c2f3b364554eb55589e1196e0d22
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2024-05-28 21:32:57 -0300

    creacion del microservicio Gateway

--- my-app/microservice-gateway/src/main/java/com/microservice/gateway/MicroserviceGatewayApplication.java
+++ my-app/microservice-gateway/src/main/java/com/microservice/gateway/MicroserviceGatewayApplication.java
@@ -2,6 +2,9 @@ package com.microservice.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
 
 @SpringBootApplication
 public class MicroserviceGatewayApplication {
