e5b4859940e22938720aaec9f694c062b00a67a3
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2024-05-29 13:19:41 -0300

    creacion del microservicio Config

--- my-app/microservice-course/src/main/java/com/microservice/client/StudentClient.java
+++ my-app/microservice-course/src/main/java/com/microservice/client/StudentClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name ="microservice-student", url = "localhost:8090/api/student")
+@FeignClient(name ="microservice-student", url = "localhost:8080/api/student")
 public interface StudentClient {
     @GetMapping("/search-my-course/{idCourse}")
     List<StudentDTO> finAllStudentByCourse(@PathVariable Long idCourse);
